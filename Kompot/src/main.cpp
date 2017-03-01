39ded1b07b352965c141aa96a82e9521f4ed38f1
Duranskinho <duranski17@gmail.com>
2017-03-02 08:26:30 +1300

    loaded bmp image + optmizations

--- Kompot/src/main.cpp
+++ Kompot/src/main.cpp
@@ -5,57 +5,97 @@
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
+//create window pointer
+SDL_Window* window = NULL;
+SDL_Surface* screenSurface = NULL;
+SDL_Surface* imageSurface = NULL;
+
 bool init();
+void render();
 bool loadShit();
 void shutdown();
 
 int main(int argc,char** argv)
 {
-	//create window pointer
-	SDL_Window* window;
+	//initialize and create the window
+	init();
 
-	//create the screen surface
-	SDL_Surface* screenSurface = NULL;
+	//load everything
+	loadShit();
 
-	//initialize SDL
-	SDL_Init(SDL_INIT_VIDEO);
-
-	//create window
-	window = SDL_CreateWindow("The damn window title", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-							  SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-
-	//get the window surface
-	screenSurface = SDL_GetWindowSurface(window);
-
-	//fill the surface to a desired color
-	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 255, 0, 0));
-
-	//update the surface
-	SDL_UpdateWindowSurface(window);
+	//draw everything
+	render();
 
 	//wait a few seconds
-	SDL_Delay(3000);
+	SDL_Delay(7000);
 
-	//destroy the window
-	SDL_DestroyWindow(window);
-
-	//quit sdl
-	SDL_Quit();
+	//shutdown and free memory
+	shutdown();
 
 	return 0;
 }
 
 bool init()
 {
-	return true;
+	//initialization flag
+	bool result = true;
+
+	//initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) > 0)
+	{
+		result = false;
+	}
+
+	//create window
+	window = SDL_CreateWindow("The damn window title", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+	if (window == NULL)
+	{
+		result = false;
+	}
+
+	//get the window surface
+	screenSurface = SDL_GetWindowSurface(window);
+
+	return result;
+}
+
+void render()
+{
+	//fill the surface to a desired color
+	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 255, 0, 0));
+
+	//apply the image
+	SDL_BlitSurface(imageSurface, NULL, screenSurface, NULL);
+
+	//update the surface
+	SDL_UpdateWindowSurface(window);
 }
 
 bool loadShit()
 {
-	return true;
+	bool result = true;
+
+	//load an image
+	imageSurface = SDL_LoadBMP("res/hello_world.bmp");
+	if (imageSurface == NULL)
+	{
+		printf("could not load the image! SDL Error: %s\n",SDL_GetError());
+		result = false;
+	}
+
+	return result;
 }
 
 void shutdown()
 {
+	//deallocate memory
+	SDL_FreeSurface(imageSurface);
+	imageSurface = NULL;
 
+	//destroy the window
+	SDL_DestroyWindow(window);
+	window = nullptr;
+
+	SDL_Quit();
 }
